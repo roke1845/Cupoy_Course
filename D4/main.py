00c5df242698bce87b4e17b584ebf90c8d32489a
LAPTOP-roger <roke1845@hotmail.com>
2021-01-24 23:35:37 +0800

    Fix answer

--- D4/main.py
+++ D4/main.py
@@ -6,4 +6,13 @@ math_score = np.array([60,85,60,68,55,60])
 
 chinese_score = np.array([65,90,82,72,66,77])
 
-print(np.greater(english_score, math_score))
+en_greater_ma = np.greater(english_score, math_score)
+print(en_greater_ma)
+print(np.sum(en_greater_ma))
+
+ch_greater_ma = np.greater(chinese_score,math_score)
+print(ch_greater_ma)
+ch_greater_en = np.greater(chinese_score,english_score)
+print(ch_greater_en)
+
+print(np.logical_and(ch_greater_ma,ch_greater_en).all())
