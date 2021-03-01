ace7f5f805fa9f7fcf4fdafdb06ad13e30983c34
Yang, Roger <roger.yang@garmin.com>
2021-03-01 13:58:02 +0800

    Fix answer

--- D11/main.py
+++ D11/main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 q_df = pd.DataFrame([['male', 'teacher'], ['male', 'engineer'], ['female', None], ['female', 'engineer']],columns=['Sex','Profession'])
-q_df.ffill()
+q_df = q_df.fillna('others')
+print(q_df)
+
+pf = pd.get_dummies(q_df[['Profession']])
+q_df = pd.concat([q_df, pf], axis=1)
 print(q_df)
